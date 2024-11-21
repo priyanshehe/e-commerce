fc55abf4f67f16f53ba371c36168d282dbc355d9
priyanshehe <2024cspriyanshu17304@poornima.edu.in>
2024-11-21 18:24:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById("search-btn").addEventListener("click", function() {
+    const query = document.getElementById("search-bar").value;
+    alert("Searching for: " + query);
+   
+});
